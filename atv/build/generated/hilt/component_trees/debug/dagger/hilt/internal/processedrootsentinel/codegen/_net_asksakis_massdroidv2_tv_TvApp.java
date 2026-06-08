3b52b8924d03bf7c777df133e3ca2fb88cfd2073
sfortis <sfortis@gmail.com>
2026-06-08 10:00:38 +0300

    gitignore: ignore every module's build/ output and stop tracking atv/build (1642 build artifacts removed from the index)

--- atv/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_net_asksakis_massdroidv2_tv_TvApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "net.asksakis.massdroidv2.tv.TvApp"
-)
-public final class _net_asksakis_massdroidv2_tv_TvApp {
-}
